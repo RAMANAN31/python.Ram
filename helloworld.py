61bdd93c31ee981314a1653c94e794a3a7649b6e
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-07-12 19:39:37 +0530

    Updatebasic/op.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
 print('hello world)
+ print(100+90)
+   # prints=100+90=190 ,here integer addition occours,then the output is displayed
+      print('100+90')
+      #here it prints 100+90 only as it is given inside a single quote,it becomes a string
+      #name ,color are the variable ,input function (built in) will take in  your input
+      name = input("whats your name?")
+color= input("favourite color")
+#for printing output
+print(name)
+print(color)
+print('hi'+name +'!'+color+'is a good color')#prints all the individual strings together
