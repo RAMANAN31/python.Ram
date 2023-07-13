ea3279714fc110df5206533cf75da2f7a74a78d8
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-07-13 12:45:09 +0530

    Create user_def_fun.py

---
+++ user_def_fun.py
@@ -0,0 +1,37 @@
+def addfive(x):
+  print(10+x)
+#user  defined function
+addfive(30)
+# 40 output 30+10
+#printing string
+def greet(name)
+print('hi' + name)
+ greet("josh")
+# hi josh
+
+
+def spuare(x);
+return x*x # operartion that we are going to perform
+result=square(5)#tells the sq of num 5 and store in result
+print(result)
+#25
+anothertime=square(result)
+print(anothertime)#end of function
+#625
+
+ def sumofsq(x,y): 
+  sq1=x*x
+  sq2=y*y
+  return sq1+sq2
+  result=sumofsq(4,5)
+  print(result)#end of function
+
+def israining():
+    raining=input('is it raining?')
+  return raining
+monday=israining()
+#output 'is it raining? (no) -(user given text)
+print(monday)
+# no
+
+  
