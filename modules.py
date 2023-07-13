525bb0f47a9f8e9cd0bc78ccb9ea61a0edee642b
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-07-13 20:58:33 +0530

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,25 @@
+import calender
+cal=calender.month(2019,7)# 7th month of 2019
+print(cal)# prints the calender of july 2019
+
+
+import math# lets us use various math functions
+result=math.sqrt(49)
+print(result)# returns square root
+
+import random # gives us random numbers ina given range
+result= random.randint(1,100)
+print(result)
+# or u can give your data and take out the random values
+movies=[tarzan,lone ranger,aladin,pirates of the caribbean,looper]
+
+
+res=random.choice(movies)
+print(res)
+#can also randomly rearrange the sequence
+
+deck=[1,23,44,3]
+
+random.sequence(deck)
+print(deck)
+# now shuffled deck will be printed
