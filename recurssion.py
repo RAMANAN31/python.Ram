74dd23da835de090f13c1e4beff40717c825495d
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-07-16 20:40:05 +0530

    Create recurssion.py

---
+++ recurssion.py
@@ -0,0 +1,22 @@
+def factorial(n):
+  """Return the factioral of integer n."""
+if n==1:
+  return 1;
+else:
+  return n*factorial(n-1)
+
+def sum_digits(n)
+  """sum of digits"""
+ if n<10:
+   return n
+ else:
+   before_last=n//10
+   last=n%10
+   return sum_digits(before_last)+last
+   # Note that integer division, denoted by the double forward slash, expresses the result of the division as 
+#an integer by cutting off decimal points and everything after the decimal points if the original result is not an integer. 
+#So you can get all the digits before the last digit of N by doing N double forward slash 10.
+
+
+
+
