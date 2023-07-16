084b110e3c51456e62fe5b343e696938d0398925
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-07-16 20:40:40 +0530

    Create oop.py

---
+++ oop.py
@@ -0,0 +1,21 @@
+In Python, an instance refers to an individual occurrence or occurrence of a class. When you create an object from a class, that object is called an instance of the class. The class serves as a blueprint, and instances are actual objects created based on that blueprint. Each instance has its own unique state and behavior.
+
+Here's an example to illustrate instances in Python:
+# Define a simple class named "Person"
+class Person:
+    # The __init__ method is a constructor that initializes the instance variables
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # A method to display information about the person
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+# Create instances of the class "Person"
+person1 = Person("Alice", 30)
+person2 = Person("Bob", 25)
+
+# Call the method to display information for each person
+person1.display_info()  # Output: Name: Alice, Age: 30
+person2.display_info()  # Output: Name: Bob, Age: 25
